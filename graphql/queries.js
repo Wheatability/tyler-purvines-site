ecfb88a9fe5cab7ef29b7ba02b88fb69f7774cb5
Tyler Purvines <tylerpurvines@gmail.com>
2022-05-02 19:12:16 +0000

    Add initial homepage query

---
+++ graphql/queries.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+const GET_HOMEPAGE = gql`
+    query {
+        homepage {
+            data {
+                attributes {
+                    Hero {
+                        Heading
+                        Subheading
+                    }
+                }
+            }
+        }
+    }`;
+
+export { GET_HOMEPAGE };
