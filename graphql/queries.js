8ba3cafbee3eb081ccdea54fd5761b80c2c04557
Tyler Purvines <tylerpurvines@gmail.com>
2022-05-03 00:48:31 +0000

    Prettify homepage query

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,17 +1,18 @@
 import { gql } from '@apollo/client';
 
 const GET_HOMEPAGE = gql`
-    query {
-        homepage {
-            data {
-                attributes {
-                    Hero {
-                        Heading
-                        Subheading
-                    }
-                }
-            }
+  query {
+    homepage {
+      data {
+        attributes {
+          Hero {
+            Heading
+            Subheading
+          }
         }
-    }`;
+      }
+    }
+  }
+`;
 
 export { GET_HOMEPAGE };
