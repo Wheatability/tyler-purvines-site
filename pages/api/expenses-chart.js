529ff2cabe8e7244fb25842c32a92bcef52b18ec
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-01 23:39:04 +0000

    Add expenses chart API route

--- pages/api/expenses-chart.js
+++ pages/api/expenses-chart.js
@@ -1,32 +1,32 @@
 export default function handler(req, res) {
-    res.status(200).json([
-        {
-            "day": "mon",
-            "amount": 17.45
-        },
-        {
-            "day": "tue",
-            "amount": 34.91
-        },
-        {
-            "day": "wed",
-            "amount": 52.36
-        },
-        {
-            "day": "thu",
-            "amount": 31.07
-        },
-        {
-            "day": "fri",
-            "amount": 23.39
-        },
-        {
-            "day": "sat",
-            "amount": 43.28
-        },
-        {
-            "day": "sun",
-            "amount": 25.48
-        }
-    ])
+  res.status(200).json([
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ])
 }
