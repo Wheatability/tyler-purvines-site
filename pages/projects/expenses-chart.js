66115f3a6cd8a785298d6dd85d2948b025c81f55
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-02 02:07:11 +0000

    Fetch data from api

--- pages/projects/expenses-chart.js
+++ pages/projects/expenses-chart.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-export default function ExpensesChart() {
+function ExpensesChart({ data }) {
   return (
     <div>
       <Head>
@@ -8,9 +8,19 @@ export default function ExpensesChart() {
         <meta name="robots" content="noindex"></meta>
       </Head>
 
-      <main>
-        <h1>test</h1>
+      <main className='bg-ec-cream h-screen w-screen'>
+
       </main>
     </div>
   );
 }
+
+export async function getServerSideProps() {
+  const api = process.env.API_URL;
+  const res = await fetch(`${api}/api/expenses-chart`);
+  const data = await res.json();
+
+  return { props: { data } }
+}
+
+export default ExpensesChart;
