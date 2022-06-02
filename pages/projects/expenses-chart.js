fec3bb54ad3d1a396704d9acc7f46198df5afdf6
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-02 20:27:46 +0000

    Add DM Sans Google font

--- pages/projects/expenses-chart.js
+++ pages/projects/expenses-chart.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import Head from 'next/head';
 
 function ExpensesChart({ data }) {
@@ -6,12 +7,15 @@ function ExpensesChart({ data }) {
       <Head>
         <title>Expenses Chart</title>
         <meta name="robots" content="noindex"></meta>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
 
       <main className='bg-ec-cream h-screen w-screen'>
 
       </main>
-    </div>
+    </div >
   );
 }
 
