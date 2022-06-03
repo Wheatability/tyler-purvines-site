fe2d7feba24d05f0d3be51e472d3f8b9273a0d11
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-03 18:20:17 +0000

    Add chart component

--- components/expenses-chart/ec-spending.js
+++ components/expenses-chart/ec-spending.js
@@ -1,10 +1,12 @@
+import ECChart from "./ec-chart";
+
 function ECSpending({ days }) {
   return (
     <>
       <div id="spending" className="flex flex-col justify-between h-[421px] space-y-6 bg-ec-card-white rounded-[10px] p-[20px]">
         <p className="text-ec-lg text-ec-brown-700 font-bold">Spending - Last 7 Days</p>
         <div className="h-[225px] border-b-2 border-[#F8E9DD] pb-6 pt-6">
-          {/* Bar Chart */}
+          <ECChart days={days} />
         </div>
         <div className="flex flex-col justify-between space-y-[3px]">
           <div className="flex flex-col">
