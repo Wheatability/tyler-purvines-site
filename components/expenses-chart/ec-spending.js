fdf2ceeacae5b4190f6a598f0ccbedbcd3253761
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-03 23:34:43 +0000

    Fix capitalization on my d

--- components/expenses-chart/ec-spending.js
+++ components/expenses-chart/ec-spending.js
@@ -4,7 +4,7 @@ function ECSpending({ days }) {
   return (
     <>
       <div id="spending" className="flex flex-col justify-between h-[421px] md:h-[511px] space-y-6 bg-ec-card-white rounded-[10px] md:rounded-[20px] p-[20px] md:p-[40px] md:pt-[32px]">
-        <p className="text-ec-lg md:text-ec-xxl text-ec-brown-700 font-bold">Spending - Last 7 Days</p>
+        <p className="text-ec-lg md:text-ec-xxl text-ec-brown-700 font-bold">Spending - Last 7 days</p>
         <div className="h-[225px] md:h-[278px] border-b-2 border-[#F8E9DD] pb-6 md:pb-8 pt-6 md:pt-4">
           <ECChart days={days} />
         </div>
