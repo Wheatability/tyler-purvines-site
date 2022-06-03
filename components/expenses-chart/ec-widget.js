0b73f6220d8f575a4cc12aa3ab518a8603397ee7
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-03 23:18:07 +0000

    Finish functionality and restyle for desktop breakpoint

--- components/expenses-chart/ec-widget.js
+++ components/expenses-chart/ec-widget.js
@@ -4,7 +4,7 @@ import ECSpending from './ec-spending';
 function ECWidget({ data }) {
   return (
     <>
-      <div id="widget" className="h-[534px] w-[343px] flex flex-col space-y-[16px] font-ec-sans select-auto">
+      <div id="widget" className="h-[534px] md:h-[660px] w-[343px] md:w-[540px] flex flex-col space-y-[16px] md:space-y-[24px] font-ec-sans select-auto">
         <ECBalance />
         <ECSpending
           days={data.days} />
