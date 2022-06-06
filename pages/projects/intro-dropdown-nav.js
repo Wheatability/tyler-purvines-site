19fe2fef372aaeb47d416113a85148a3cfb48e60
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-06 15:23:58 +0000

    Initial commit

---
+++ pages/projects/intro-dropdown-nav.js
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import Head from 'next/head';
+
+function ExpensesChart({ data }) {
+  return (
+    <div>
+      <Head>
+        <title>Expenses Chart</title>
+        <meta name="robots" content="noindex"></meta>
+      </Head>
+      <main>
+        <p style={{ fontSize: "11px", position: "absolute", bottom: "0" }}>
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="#" >Tyler Purvines.</a>
+        </p>
+      </main>
+
+    </div >
+  );
+};
+
+export default ExpensesChart;
