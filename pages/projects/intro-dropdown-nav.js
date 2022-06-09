40dc40ee016863f7ea6b79ef9579881fe4821e85
Tyler Purvines <tylerpurvines@gmail.com>
2022-06-09 23:25:54 +0000

    Add Epilogue Google Font

--- pages/projects/intro-dropdown-nav.js
+++ pages/projects/intro-dropdown-nav.js
@@ -5,16 +5,22 @@ function IntroDropdownNav() {
   return (
     <div>
       <Head>
-        <title>Expenses Chart</title>
+        <title>Intro Section with Dropdown Navigation</title>
         <meta name="robots" content="noindex"></meta>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
       <main>
-        <p style={{ fontSize: "11px", position: "absolute", bottom: "0" }}>
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>.
-          Coded by <a href="#" >Tyler Purvines.</a>
-        </p>
-      </main>
+        <nav>
 
+        </nav>
+      
+      </main>
+      <p style={{ fontSize: "11px", position: "absolute", bottom: "0" }}>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#" >Tyler Purvines.</a>
+      </p>
     </div >
   );
 };
